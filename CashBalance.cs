dd00c050bf618b6ba30e9567bf62b1f388228c7e
ActiveP <patrykos65a@gmail.com>
2025-05-19 13:17:47 +0200

    Update CashBalance.cs

--- CashBalance.cs
+++ CashBalance.cs
@@ -7,6 +7,9 @@ public class CashBalance : MonoBehaviour
     public TMP_Text coinsText;
     public float coins = 999.5f;
 
+    public TMP_InputField coinsInputField;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,6 +23,24 @@ public class CashBalance : MonoBehaviour
     }
     void UpdateCoinsText()
     {
-        coinsText.text = "Coins: " + coins;
+        coinsText.text = "Coins: " + coins.ToString("0.00");
     }
+    public void BetButton()
+    {
+        float value;
+        if (float.TryParse(coinsInputField.text, out value))
+        {
+            if (value <= coins)
+            {
+                coins = coins - value;
+                UpdateCoinsText();
+            }
+            else
+            {
+                Debug.Log("za ma³o coins");
+            }
+        }
+        
+    }
+
 }
