51a84d18396b1a66f69d365b952d4308e3557f20
ActiveP <patrykos65a@gmail.com>
2025-05-20 17:20:30 +0200

    Color Betting

--- CashBalance.cs
+++ CashBalance.cs
@@ -11,10 +11,18 @@ public class CashBalance : MonoBehaviour
     public TMP_InputField coinsInputField;
     private float currentCoinsValue = 0f;
 
+    public TMP_Text redText;
+    public TMP_Text blackText;
+    public TMP_Text greenText;
+
+    public float redBet = 0f;
+    public float blackBet = 0f;
+    public float greenBet = 0f;
 
     void Start()
     {
         UpdateCoinsText();
+        redText.text = null;
     }
 
     void UpdateCoinsText()
@@ -34,22 +42,6 @@ public class CashBalance : MonoBehaviour
         }
     }
 
-    public void BetButton()
-    {
-        ValueFromInputField();
-        if (currentCoinsValue <= coins)
-        {
-            Debug.Log(currentCoinsValue);
-            coins = coins - currentCoinsValue;
-            UpdateCoinsText();
-        }
-        else
-        {
-            Debug.Log(coins);
-            Debug.Log("za ma³o coins");
-        }
-        
-    }
     public void Betx2()
     {
         ValueFromInputField();
@@ -60,6 +52,41 @@ public class CashBalance : MonoBehaviour
     {
         ValueFromInputField();
         currentCoinsValue /= 2;
+        UpdateInputCoins();
     }
+    public void ChooseBetColor(string color)
+    {
+        ValueFromInputField();
+
+        if (currentCoinsValue <= 0f || currentCoinsValue > coins)
+        {
+            Debug.Log("not enough coins");
+            return;
+        }
+        coins = coins - currentCoinsValue;
+        UpdateCoinsText();
+        switch (color)
+        {
+            case "Red":
+                redBet += currentCoinsValue;
+                redText.text = redBet.ToString("0.00");
+                break;
+
+            case "Black":
+                blackBet += currentCoinsValue;
+                blackText.text = blackBet.ToString("0.00");
+                break;
+
+            case "Green":
+                greenBet += currentCoinsValue;
+                greenText.text = greenBet.ToString("0.00");
+                break;
+
+            default:
+                Debug.LogWarning("Nieznany kolor zak³adu: " + color);
+                break;
+        }
+    }
+
 
 }
